ec5e0cb178d063ad80d3b47ac44b3f71abf767ea
Joshitha Sai Uppalapati <uppalapatijoshithasai@gmail.com>
2025-10-03 10:46:13 -0400

    Fix import path for HF: add __init__.py and set PYTHONPATH to src

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
-from src.meddx.serve import ui_app
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+from meddx.serve import ui_app  # this imports and runs the Streamlit script
